c17c157a3b6dde600042f2bc3ff90f87de2a07d9
olehsul <oleh.sul@gmail.com>
2018-11-05 14:25:45 +0200

    Initial commit

---
+++ src/main/java/com/owu/geekhub/GeekhubApplication.java
@@ -0,0 +1,12 @@
+package com.owu.geekhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekhubApplication.class, args);
+    }
+}
---
+++ src/test/java/com/owu/geekhub/GeekhubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.owu.geekhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeekhubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
