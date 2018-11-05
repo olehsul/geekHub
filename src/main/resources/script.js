20452c848d0d2595232d0b09db7ddaf8e18b150b
TLU3T <btovaryanskiy@gmail.com>
2018-11-05 20:35:43 +0200

    html_01a

---
+++ src/main/resources/script.js
@@ -0,0 +1,6 @@
+function isValidEmail(Email) {
+    return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(Email);
+}
+function isValidText(Text) {
+    return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(Text);
+}
