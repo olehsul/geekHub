32f7b369b4344e17a0a1b2afb12d179eb08016a2
mnazar <Mikel.nazar91@gmail.com>
2018-11-17 23:07:47 +0200

    Validate regex

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,33 +11,80 @@ $('#passwordId, #confirmPasswordId').on(function () {
           return varName.test(String(name))})
       }*/
 
-/* $('#exampleDropdownFormEmail1').on('exampleDropdownFormEmail1', function () {
-     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-     return re.test(String(email).toLowerCase())})*/
+
 
 function validate(e) {
     var fname = $("#fname").val();
     var lname = $("#lname").val();
-    var email1 = $("#exampleDropdownFormEmail1").val();
+    var email = $("#exampleDropdownFormEmail1").val();
     var password = $("#passwordId").val();
     var confirmPassword = $("#confirmPasswordId").val();
 
-    if (nameValid(fname) == false) {
-        console.log("sgsewefw")
+    if (firstNameValid(fname) == false) {
+        console.log("false first name")
         e.preventDefault();
-
-
         }
 
+
+    if (lastNameValid(lname) == false) {
+        console.log("false first name")
+        e.preventDefault();
+    }
+
+    if (emailValid(email) == false) {
+        console.log("false email")
+        e.preventDefault();
+    }
+
+    if (passwordValid(password) == false) {
+        console.log("false password")
+        e.preventDefault();
+    }
+
+    if (confirmPasswordValid(confirmPassword) == false) {
+        console.log("false confirm password")
+        e.preventDefault();
+    }
+
 }
 
 
-function nameValid(name) {
-    console.log("sdgs")
-    nemeRegex = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/;
-    return nemeRegex.test(name);
+function firstNameValid(name) {
+    console.log("true first name")
+    firstNameRegex = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+    return firstNameRegex.test(name);
 
 }
+
+function lastNameValid(name) {
+    console.log("true last name")
+    lastNameRegex = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+    return lastNameRegex.test(name);
+
+}
+
+function emailValid(email) {
+    console.log("true email")
+    emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return emailRegex.test(email);
+
+}
+
+
+function passwordValid(password) {
+    console.log("true password")
+    passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/;
+    return passwordRegex.test(password);
+
+}
+
+function confirmPasswordValid(password) {
+    console.log("true confirm password")
+    confirmPasswordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/;
+    return confirmPasswordRegex.test(password);
+
+}
+
 
 
 
