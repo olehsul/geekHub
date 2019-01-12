26a6c86b61e251c3c7e738a04aa4ae73768d33f6
Sviatoslav <shot.ngh@gmail.com>
2019-01-12 14:22:28 +0200

    added user API

--- src/main/java/com/owu/geekhub/controllers/TestRestAPIs.java
+++ src/main/java/com/owu/geekhub/controllers/TestRestAPIs.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 public class TestRestAPIs {
 
